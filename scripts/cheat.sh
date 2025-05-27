0df958d2631000a696a455109cd79e8fba452e24
zepzeper <wouterschiedam98@gmail.com>
2025-05-27 17:20:28 +0200

    batman

---
+++ scripts/cheat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+languages=`echo "php lua nvim rust" | tr ' ' '\n'`
+
+selected=`printf "$languages" | fzf`
+
+read -p "query: " query
+
+if printf $languages | grep -qs $selected; then
+    tmux neww bash -c "curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done"
+else
+    echo "not found"
+fi
+
+
