b19e752cd2af702a2708b8e816ccd02f791cd3c4
Wouter van Krugten <wouterschiedam98@gmail.com>
2026-01-17 13:01:02 +0100

    update

--- scripts/.local/bin/rbw.sh
+++ scripts/.local/bin/rbw.sh
@@ -4,7 +4,7 @@
 rbw unlock
 
 # Select item and action in one go
-SELECTION=$(rbw ls | awk '{print $0 " [password]\n" $0 " [username]"}' | fzf)
+SELECTION=$(rbw ls --fields name,user | awk -F'\t' '{print $1 " (" $2 ") [password]\n" $1 " (" $2 ") [username]"}' | fzf)
 
 if [ -z "$SELECTION" ]; then
     notify-send "Cancelled" "No item selected"
@@ -16,11 +16,11 @@ ACTION=$(echo "$SELECTION" | grep -o '\[.*\]' | tr -d '[]')
 
 case "$ACTION" in
     "password")
-        rbw get "$ITEM" | wl-copy
+        rbw get "$ITEM" | wl-copy --paste-once
         notify-send "Password Copied" "$ITEM"
         ;;
     "username")
-        rbw get --field username "$ITEM" | wl-copy
+        rbw get --field username "$ITEM" | wl-copy --paste-once
         notify-send "Username Copied" "$ITEM"
         ;;
 esac
