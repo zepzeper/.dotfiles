e36a327a51d4b536941b3aaee42faf822e3fc9e6
Wouter van Krugten <wouterschiedam98@gmail.com>
2026-01-22 11:22:56 +0100

    Update: nvim conf

--- scripts/.local/bin/cheat.sh
+++ scripts/.local/bin/cheat.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/bash
 
-selected=`cat ~/personal/.dotfiles/scripts/resources/.tmux-cht-languages ~/personal/.dotfiles/scripts/resources/.tmux-cht-command | fzf`
+selected=`cat /home/zepzeper/.local/bin/resources/.tmux-cht-languages /home/zepzeper/.local/bin/resources/.tmux-cht-command | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter query: " query
 
-if grep -qs "$selected" ~/personal/.dotfiles/scripts/resources/.tmux-cht-languages; then
+if grep -qs "$selected" /home/zepzeper/.local/bin/resources/.tmux-cht-languages; then
     tmux neww bash -c "curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
